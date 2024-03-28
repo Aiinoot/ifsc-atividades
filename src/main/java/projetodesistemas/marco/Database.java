c4f81a68e890e28acf4347efe243fa44d6fb1d33
Aiinoot <antoniope1017@gmail.com>
2024-03-27 21:05:17 -0300

    realiza uma demonstração de singleton abrindo um banco de dados, impedindo outras instância.

--- src/main/java/projetodesistemas/marco/Database.java
+++ src/main/java/projetodesistemas/marco/Database.java
@@ -1,4 +1,20 @@
 package projetodesistemas.marco;
 
 public class Database {
+
+    private static Database database;
+
+    private Database() {
+    }
+
+    public void getConnection(){
+        System.out.println("Conexão Aberta");
+    }
+    public static synchronized Database getInstance(){
+        if (database == null){
+            database = new Database();
+        }
+        return database;
+    }
+
 }
---
+++ src/main/java/projetodesistemas/marco/Main.java
@@ -0,0 +1,9 @@
+package projetodesistemas.marco;
+
+import javax.xml.crypto.Data;
+
+public class Main {
+    public static void main(String[] args) {
+        Database.getInstance().getConnection();
+    }
+}
